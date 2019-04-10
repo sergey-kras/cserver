57863d3f2670f269dcb70c61e0117237e43eb914
SergeyKR <vip.41243@gmail.com>
2019-04-11 00:42:29 +0700

    https://trello.com/c/2mTqn7YA

---
+++ config.js
@@ -0,0 +1,12 @@
+const
+    CLIENT_ID = process.env.CLIENT_ID,
+    CLIENT_SECRET = process.env.CLIENT_SECRET,
+    REDIRECT_URI = process.env.REDIRECT_URI,
+    FRONTENT_URI = process.env.FRONTENT_URI;
+
+module.exports = {
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI,
+    FRONTENT_URI
+}
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+require('dotenv').config();
 const app = require('./app/routes');
+console.log(process.env.CLIENT_SECRET);   
 app.listen(4200, () => { console.log('App listen on 4200');});
